2f76c0f2e76b464423b6d6a9357ec7e692381cdc
Artyom <artyomantonenko@gmail.com>
2026-02-06 23:33:21 +0100

    updated password schema

--- src/components/form/sign-up/index.ts
+++ src/components/form/sign-up/index.ts
@@ -5,12 +5,8 @@ export const SignUpSchema = z.object({
     email: z.string().email("You must give a valid email"),
     password: z
         .string()
-        .min(8, { message: "Your password must be atleast 8 characters long" })
+        .min(8, { message: "Your password must be at least 8 characters long" })
         .max(64, {
-            message: "Your password can not be longer then 64 characters long",
-        })
-        .refine(
-            (value) => /^[a-zA-Z0-9_.-]*$/.test(value ?? ""),
-            "password should contain only alphabets and numbers",
-        ),
+            message: "Your password can not be longer than 64 characters",
+        }),
 })
